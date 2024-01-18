6fcc6d442f31294afa658dee4b7b731e318ea88e
Arya Murray <themagician948@gmail.com>
2024-01-17 20:40:09 -0500

    added store

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,39 @@
 import { error } from "console"
 import { course } from "../types.js"
 import fs from "fs/promises"
+import path from "path"
 
 
-export async function storeCourse(course:course[], subject:string){
-    fs.writeFile(`/store/${subject}.json`,JSON.stringify(course, null, 2))
+export async function storeDepartment(courses:course[], subject:string){
+    await fs.mkdir(`store`,{recursive:true})
+    await fs.writeFile(`${process.cwd()}/store/${subject}.json`,JSON.stringify(courses, null, 2))
 }
 
 export async function readStore(subject:string):Promise<course[]>{
-    const data = await fs.readFile(`/store/${subject}.json`, "utf-8")
+    const data = await fs.readFile(`${process.cwd()}/store/${subject}.json`, "utf-8").catch()
     return JSON.parse(data) as course[]
+}
+
+//Returns an array of updated nbrs
+export async function compareCourses(department: string, newCourses: course[]): Promise<string[]> {
+    try {
+        const departmentStore = await readStore(department);
+        let updatedNbrs: string[] = [];
+
+        departmentStore.forEach((course) => {
+            const matchingCourse = newCourses.find(newCourse => newCourse.name === course.name);
+            if (matchingCourse) {
+                course.components.forEach((component) => {
+                    const matchingComponent = matchingCourse.components.find(newComponent => newComponent.nbr === component.nbr);
+                    if (matchingComponent && matchingComponent.status !== component.status) {
+                        updatedNbrs.push(matchingComponent.nbr);
+                    }
+                });
+            }
+        });
+
+        return updatedNbrs;
+    } catch (error) {
+        console.error(error);
+    }
 }
